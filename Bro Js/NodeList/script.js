b5e7b92b56f33e173a089227ee73509b868645af
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-05-14 20:45:38 +0300

    Remove NodeList element

--- Bro Js/NodeList/script.js
+++ Bro Js/NodeList/script.js
@@ -1,7 +1,6 @@
 
 let buttons = document.querySelectorAll(".myButtons");
 
-console.log(buttons);
 
 buttons.forEach(button => {
     button.addEventListener("click", event => {
@@ -26,3 +25,16 @@ const newButton = document.createElement("button");
 newButton.textContent = "Button N";
 newButton.classList = "myButtons";
 document.body.appendChild(newButton);
+
+buttons = document.querySelectorAll(".myButtons");
+console.log(buttons);
+
+// Remove an element
+
+buttons.forEach(button => {
+    button.addEventListener("click", event =>{
+        event.target.remove();
+        buttons = document.querySelectorAll(".myButtons");
+        console.log(buttons);
+    })
+});
